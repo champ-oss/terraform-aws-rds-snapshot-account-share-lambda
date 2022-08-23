e34ed1277fc3fde8e290c80ec3f28af02016077a
Phillip Langley <philliplangley@champmbplangley.lan>
2022-08-23 15:35:34 -0500

    resolve linting and add iam policy

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,7 +16,7 @@ module "vpc" {
 }
 
 module "this" {
-  source                              = "../../"
-  private_subnet_ids                  = module.vpc.private_subnets_ids
-  vpc_id                              = module.vpc.vpc_id
+  source             = "../../"
+  private_subnet_ids = module.vpc.private_subnets_ids
+  vpc_id             = module.vpc.vpc_id
 }
--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 module "rds_snapshot_account_share_lambda" {
   source              = "github.com/champ-oss/terraform-aws-lambda.git?ref=v1.0.71-2e2e648"
   git                 = var.git
-  name                = "rds-snapshot-account-share-${random_string.identifier[0].result}"
+  name                = "rds-snapshot-account-share-${random_string.identifier.result}"
   tags                = merge(local.tags, var.tags)
   description         = "rds-snapshot-account-share lambda function"
   enable_cw_event     = var.enable_lambda_cw_event
@@ -10,7 +10,7 @@ module "rds_snapshot_account_share_lambda" {
   private_subnet_ids  = var.private_subnet_ids
   sync_image          = true
   sync_source_repo    = "champtitles/rds-snapshot-account-share"
-  ecr_name            = "rds-snapshot-account-share-${random_string.identifier[0].result}"
+  ecr_name            = "rds-snapshot-account-share-${random_string.identifier.result}"
   ecr_tag             = var.rds_snapshot_account_share_docker_tag
   vpc_id              = var.vpc_id # eni delete resource bug https://github.com/hashicorp/terraform-provider-aws/issues/10329
   environment = {
