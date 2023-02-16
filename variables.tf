9c583bbc78c9c2028dfd9d6410051b364beabca5
Phillip Langley <philliplangley@champmbplangley.lan>
2023-02-16 14:31:09 -0600

    bugfix snapshot check

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "schedule_expression" {
 variable "rds_snapshot_account_share_docker_tag" {
   description = "Docker tag of rds snapshot share code to deploy"
   type        = string
-  default     = "ca017c5415359db69078fd1b279fea318644ecc6"
+  default     = "9340c84362e529f06fb67f7be7fc41fcb20079a5"
 }
 
 variable "aws_shared_account" {
