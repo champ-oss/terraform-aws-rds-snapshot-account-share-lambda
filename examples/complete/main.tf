b51ebb2a74921402c8c76b80bedff680810f4743
Justin Reeves <reeves122@gmail.com>
2024-05-16 15:12:42 -0500

    add TF required providers

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.30.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
